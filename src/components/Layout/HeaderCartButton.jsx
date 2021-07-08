c3521d8ee23fc45aba679c4eb8df0d7542dfec13
Alex <Aleks031994@Mail.ru>
2021-07-08 16:43:08 +0300

    completed reducer

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {useContext} from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css'
+import Cart from "../Cart/Cart";
 
 const HeaderCartButton = ({onClick}) => {
+
+    const cartCtx = useContext(CartContext)
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount
+    }, 0)
+
     return (
         <button onClick={onClick} className={styles.button}>
             <span className={styles.icon}>
@@ -10,7 +20,7 @@ const HeaderCartButton = ({onClick}) => {
             </span>
             <span>Your Cart</span>
             <span className={styles.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
